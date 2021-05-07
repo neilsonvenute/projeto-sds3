56eb81f85a88958a6a6294f5c3473e5401529836
Neilson Venute <neilsonvenute@gmail.com>
2021-05-07 19:40:36 -0300

    Home, navigation

--- front-end/src/Routes.tsx
+++ front-end/src/Routes.tsx
@@ -1,19 +1,19 @@
 import Dashboard from 'pages/Dashboard';
 import Home from 'pages/Home';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Routes = ()=>{
-    return(
-       <BrowserRouter>
-        <Switch>
-            <Route path="/" exact>
-                <Home/>
-            </Route>
-            <Route path="/dashboard" exact>
-                <Dashboard/>
-            </Route>
-        </Switch>
-       </BrowserRouter>
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" exact>
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
--- front-end/src/pages/Dashboard/index.tsx
+++ front-end/src/pages/Dashboard/index.tsx
@@ -4,31 +4,31 @@ import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
 
-const Dashboard = ()=>{
-    return(
+const Dashboard = () => {
+    return (
         <>
-      <Navbar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+            <Navbar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as Vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas as Vendas</h5>
+                        <DonutChart />
+                    </div>
+                </div>
+                <div className="py-3">
+                    <h2 className="text-primary">Todas vendas</h2>
+                </div>
+                <DataTable />
+            </div>
+            <Footer />
+        </>
     );
 }
 
-export default Dashboard; 
+export default Dashboard;
